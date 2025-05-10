19afe14ea87260747dca21ea1b9cd2837742dec3
Luis Ricardo <luisvega89@aragon.unam.mx>
2025-05-10 10:20:58 -0600

    Add files via upload

---
+++ 07052025/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const routes = require("./routes/routes");//importamos las rutas
+
+const app = express();
+const port = 3024;
+//sirve para archivos estaticos en css, img, etc
+app.use(express.static(path.join(__dirname,"public")));
+
+app.use("/",routes);
+
+app.listen(port,()=>{
+    console.log(`http://localhost:${port}`);
+});
